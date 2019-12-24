6905a232cd038159bc91dfe7d661ee1dd31b1b42
chonepieceyb <34810016+chonepieceyb@users.noreply.github.com>
2019-12-24 15:47:24 +0800

    更新了一些论文

--- ImageProcessing/Retinex/retinex.py
+++ ImageProcessing/Retinex/retinex.py
@@ -98,15 +98,17 @@ def retinex_ssr(input,gauss_c=80,kernal_size=[3,3]):
     # input_L = cv.GaussianBlur(input,(h,w),gauss_c)
     # input_L = np.multiply(input_L,kernal)
     # input_L = np.average(input_L)
-    input_L = cv.filter2D(input,-1,kernal)
+    input_L = cv.filter2D(input_L,-1,kernal)
     input = np.clip(input,0.000001,255)
-    input_L = np.clip(input_L, 0.000001, 255)
+    input_L = np.clip( input_L , 0.000001, 255)
     input = np.log(input)
     input_L= np.log(input_L)
     output = input - input_L
     #output = np.exp(output)
-    output = linear_stretch(output)
-    output = np.array(output,dtype= np.uint8)
+    #output = linear_stretch(output)
+    #output = np.array(output,dtype= np.uint8)
+    output  = input + 6*output
+    output = np.exp(output)
     return np.resize(output,shape)
 
 
@@ -125,8 +127,8 @@ if __name__ =='__main__':
     ################# 图片读取模块
     img_name = os.path.split(args.img)[-1]
     img_name, ext = os.path.splitext(img_name)
-    #input = cv.imread(args.img, flags=cv.IMREAD_GRAYSCALE) #读取灰度图片
-    input = cv.imread(args.img, flags=cv.IMREAD_GRAYSCALE) #读取rgb图片
+    input = cv.imread(args.img, flags=cv.IMREAD_GRAYSCALE) #读取灰度图片
+    #input = cv.imread(args.img, flags=cv.IMREAD_COLOR) #读取rgb图片
     ########################## 处理模块
     output = retinex_ssr(input,float(args.c),[input.shape[0],input.shape[1]])
     #########图片输出模块
